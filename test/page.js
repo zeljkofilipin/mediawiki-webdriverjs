010632d895aa4091f9be31177698e12069f31cdf
Željko Filipin <zeljko.filipin@gmail.com>
2016-11-18 17:51:24 +0100

    Page should be created

---
+++ test/page.js
@@ -0,0 +1,25 @@
+var assert = require( 'assert' ),
+	webdriver = require( 'selenium-webdriver' ),
+	By = webdriver.By,
+	test = require( 'selenium-webdriver/testing' );
+
+test.describe( 'Page', function () {
+	var driver;
+
+	test.beforeEach( function () {
+		driver = new webdriver.Builder()
+		.forBrowser( 'firefox' )
+		.build();
+	} );
+
+	test.afterEach( function () {
+		driver.quit();
+	} );
+
+	test.it( 'should be created', function () {
+		driver.get( 'http://127.0.0.1:8080/wiki/Does_not_exist' );
+		driver.findElement( By.css( 'li#ca-edit a' ) ).isDisplayed().then( function ( displayed ) {
+			assert( displayed );
+		} );
+	} );
+} );
